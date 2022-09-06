3677cc7dc6130be0fbcb925433fcc753262d53a3
cesaregarza <40225001+cesaregarza@users.noreply.github.com>
2022-09-05 21:53:51 -0500

    work on the screenshotter

--- nerdtracker_client/screenshots/screenshotter.py
+++ nerdtracker_client/screenshots/screenshotter.py
@@ -9,12 +9,32 @@ from mss.screenshot import ScreenShot
 
 
 class Screenshotter:
+    """Screenshotter class for taking screenshots of the game continuously, with
+    a specified time interval between screenshots. The screenshots are then sent
+    to the NerdTracker_Server docker container for processing. This class does
+    not do any processing itself, it only takes screenshots and sends them to
+    the server. It does not wait for a response from the server, and it does not
+    do any processing on the server's response. The server will decide whether
+    or not to send a response, and the client will then act on that response.
+    """
+
     def __init__(
         self,
         monitor_index: int = 1,
         time_interval: int | float = 0.5,
         start_immediately: bool = False,
     ) -> None:
+        """Constructor for the Screenshotter class
+
+        Args:
+            monitor_index (int): The monitor index to take screenshots
+                from. Not zero-indexed. Defaults to 1.
+            time_interval (int | float): The time interval between
+                screenshots. Defaults to 0.5.
+            start_immediately (bool): Whether or not to start the
+                screenshotting immediately. Defaults to False. If True, the
+                screenshotting will start immediately upon instantiation.
+        """
         self.sct = mss.mss()
         self.monitor = self.sct.monitors[monitor_index]
         self.size = (self.monitor["width"], self.monitor["height"])
@@ -27,18 +47,29 @@ class Screenshotter:
             self.timer_start()
 
     def _run(self) -> None:
+        """The function that is called by the timer
+
+        This function is called upon by the timer. This function simply flips
+        the _is_running flag to False, restarts the timer, and calls the
+        process_screenshot function.
+        """
         self._is_running = False
-        self.timer_start()
         self.process_screenshot()
+        self.timer_start()
 
     def timer_start(self) -> None:
+        """Starts the timer"""
         if not self._is_running:
             self.next_call += self.time_interval
-            self._timer = Timer(self.next_call - time.time(), self._run)
+            # In case the processing takes longer than the time interval, this
+            # ensures the time left is at least 0
+            time_left = max(self.next_call - time.time(), 0.0)
+            self._timer = Timer(time_left, self._run)
             self._timer.start()
             self._is_running = True
 
     def timer_stop(self) -> None:
+        """Stops the timer"""
         if self._timer is not None:
             self._timer.cancel()
         self._is_running = False
@@ -60,11 +91,28 @@ class Screenshotter:
         self.timer_stop()
 
     def take_screenshot(self) -> npt.NDArray[np.uint8]:
+        """Takes a screenshot of the given monitor
+
+        Returns:
+            npt.NDArray[np.uint8]: The screenshot as a numpy array, without the
+                alpha channel
+        """
         grab = self.sct.grab(self.monitor)
         frame = np.array(grab)
         # Remove the alpha channel, which is always 255
         cut_frame = frame[:, :, :-1]
         return cut_frame
 
+    def send_screenshot(self, screenshot: npt.NDArray[np.uint8]) -> None:
+        """Sends the screenshot to the server for processing. Does not wait for
+        a response.
+
+        Args:
+            screenshot (npt.NDArray[np.uint8]): The screenshot to send to the
+                server.
+        """
+        pass
+
     def process_screenshot(self) -> None:
         screenshot = self.take_screenshot()
+        self.send_screenshot(screenshot)
